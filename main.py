bfcd89f6f32fdc2ee7556d43646e25e52d6cccf2
paul-michelle <paulmichellegallery@gmail.com>
2021-12-16 16:14:00 +0300

    Enable redis caching for single item request. Compare performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import io
+import re
 import aiohttp
 import redis
 from socket import socket, AF_INET, SOCK_STREAM
@@ -14,11 +15,11 @@ class Server:
     def __init__(self):
         self._server_socket = None
         self._set_server()
-        self._redis = redis.Redis()
+        self._redis_instance = redis.Redis()
 
     def _set_server(self):
         self._server_socket = socket(AF_INET, SOCK_STREAM)
-        self._server_socket.bind(('', 9999))
+        self._server_socket.bind(('', 9998))
         self._server_socket.listen(1)
 
     def _get_client_socket(self):
@@ -32,19 +33,25 @@ class Server:
         return target
 
     async def _get_or_set_cache(self, key: str) -> bytes:
-        data_cached = self._redis.get(key)
+        data_cached = self._redis_instance.get(key)
         if data_cached:
             return data_cached
         async with aiohttp.ClientSession() as session:
             response_from_service = await session.request(method='GET', url=key)
             data = await response_from_service.read()
-            self._redis.setex(key, EXPIRATION_TIME, data)
+            self._redis_instance.setex(key, EXPIRATION_TIME, data)
         return data
 
     async def _get_targeted_data(self, target: str) -> bytes:
-        data = b''
+        key = ''
         if target == '/photos':
-            data = await self._get_or_set_cache(key=BASE_TARGET_URL)
+            key = BASE_TARGET_URL
+        single_photo_request = re.search(r'^/photos/[0-9]+$', target)
+        if single_photo_request:
+            photo_id = single_photo_request.group().split('/')[2]
+            key = BASE_TARGET_URL + '/' + photo_id
+
+        data = await self._get_or_set_cache(key)
         return data
 
     @staticmethod
@@ -61,11 +68,18 @@ class Server:
         while True:
             request = sock.recv(MAX_BYTES)
             if not request:
+                sock.close()
                 break
             target = self._get_target(request=io.BytesIO(request))
             data = await self._get_targeted_data(target)
             response = self._form_valid_response(data)
             sock.send(response)
+
+    def show_cache_keys(self) -> None:
+        print(self._redis_instance.keys())
+
+    def flush_cache(self) -> None:
+        self._redis_instance.flushall()
 
 
 if __name__ == '__main__':
@@ -73,9 +87,15 @@ if __name__ == '__main__':
     try:
         asyncio.run(server.serve())
     except KeyboardInterrupt:
-        pass
+        info_to_be_flushed = input('Flush all data from redis?')
+        if info_to_be_flushed.lower() in ['y', 'yes', 'ok']:
+            server.show_cache_keys()
+            server.flush_cache()
 
 """
 Bulk writing without enabling redis: 286 ms 1.02 MB, which is 3x faster than with node.js axios in similar conditions.
 Bulk writing with redis caching: 4 ms 1.02 MB, which is 5x faster than with node.js axios in similar conditions.
+
+Single photo request handling before caching: 387 ms for 281 B of message
+After caching: 2-4 ms.
 """
