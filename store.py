8ac692a95133aa94e77dd9247fcfeebc316b09c7
paul-michelle <paulmichellegallery@gmail.com>
2021-12-17 20:33:27 +0300

    Add Retailer with get_info, sell and refresh methods

--- store.py
+++ store.py
@@ -1,7 +1,8 @@
 import random
 import redis
 from faker import Faker
-
+from pprint import pprint
+from typing import List, Tuple, Dict, Optional
 
 fake = Faker()
 
@@ -19,7 +20,7 @@ class Conveyor:
         }
 
     @classmethod
-    def produce(cls, amount: int):
+    def produce(cls, amount: int) -> List['Conveyor']:
         produced = []
         for i in range(amount):
             produced.append(cls())
@@ -36,3 +37,84 @@ class Supplier:
             for item in Conveyor.produce(amount):
                 pipe.hmset(item.hash, item.description)
             pipe.execute()
+
+
+class OutOfStockError(Exception):
+    pass
+
+
+class NotEnoughInStock(Exception):
+    """ e.g.:
+    __main__.NotEnoughInStock: Quantity available of item:1810785242 is b'27'
+    """
+    pass
+
+
+class Retailer:
+
+    def __init__(self):
+        self._redis_instance = redis.Redis(db=4)
+
+    def show_goods_available(self) -> List[bytes]:
+        return self._redis_instance.keys()
+
+    def get_item_info(self, item_id: str) -> Dict[bytes, bytes]:
+        return self._redis_instance.hgetall(item_id)
+
+    def get_all_info(self) -> List[Tuple[bytes, Dict[bytes, bytes]]]:
+        """ Receive a list of tuples with info.
+        (b'item:960156607',
+          {b'date': b'2017-07-01',
+           b'designer': b'Gabriela Carter',
+           b'price': b'87',
+           b'purchased': b'0',
+           b'quantity': b'29'})
+        """
+        return [(item_id, self.get_item_info(item_id.decode('utf-8')))
+                for item_id in self.show_goods_available()]
+
+    def sell(self, item_id: str, quantity_to_sell: int) -> Optional[str]:
+        """ e.g.:
+        'Sold 27 of item:1810785242. Revenue: 2322'
+        """
+        with self._redis_instance.pipeline() as pipe:
+            error_count = 0
+            while True:
+                try:
+                    pipe.watch(item_id)
+                    quantity_in_stock: bytes = self._redis_instance.hget(item_id, 'quantity')
+                    if quantity_in_stock < str(quantity_to_sell).encode('utf-8'):
+                        raise NotEnoughInStock(f'Quantity available of {item_id} is {quantity_in_stock}')
+                    if quantity_in_stock > b'0':
+                        pipe.multi()
+                        pipe.hincrby(item_id, 'quantity', -quantity_to_sell)
+                        pipe.hincrby(item_id, 'purchased', quantity_to_sell)
+                        pipe.execute()
+                        revenue = quantity_to_sell * int(self._redis_instance.hget(item_id, 'price').decode('utf-8'))
+                        return f'Sold {quantity_to_sell} of {item_id}. Revenue: {revenue}'
+                    pipe.unwatch()
+                    raise OutOfStockError(f'{item_id} is out of stock. Contact the supplier')
+                except redis.WatchError:
+                    error_count += 1
+                    print(f'Tried to sell {item_id}, failed {error_count} time(s).')
+
+    def refresh(self) -> str:
+        """
+        retailer = Retailer()
+        retailer.refresh()
+        'Everything up-to-date' | 'Info refreshed. Number of entries deleted: 2'
+        """
+        try:
+            with self._redis_instance.pipeline() as pipe:
+                entries_deleted: int = 0
+                pipe.multi()
+                for item_id, _ in self.get_all_info():
+                    if self._redis_instance.hget(item_id, 'quantity') <= b'0':
+                        self._redis_instance.delete(item_id)
+                        entries_deleted += 1
+                pipe.execute()
+                if entries_deleted:
+                    return f'Info refreshed. Number of entries deleted: {entries_deleted}'
+                return f'Everything up-to-date'
+        except redis.RedisError as e:
+            return f'Redis: Exception occurred. {e}'
